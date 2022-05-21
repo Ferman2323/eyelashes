7d6db0f3e0d3c2bd80e8652c6e2e89f44ad5741d
SasaFermanovic <57187077+Ferman2323@users.noreply.github.com>
2022-05-21 22:27:31 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.navbar-list');
+let navlink = document.querySelector('.nav-link');
 
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('active');
     nav.classList.toggle('active');
-    document.querySelector('body').classList.toggle('overflow');
-
 })
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    nav.classList.remove("active");
+  })
+);
